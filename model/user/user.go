60a36c6bb031b020b91402b8b83440bcb81c7515
lusa <tolstovi056@gmail.com>
2026-04-22 19:24:54 +0300

    V4

--- model/user/user.go
+++ model/user/user.go
@@ -2,9 +2,11 @@ package user
 
 import (
 	"errors"
+	"sync"
 )
 
 type User struct {
+	mu      sync.Mutex
 	ID      string
 	Name    string
 	Balance float64
@@ -19,10 +21,14 @@ func NewUser(id, name string, balance float64) *User {
 }
 
 func (u *User) Deposit(amount float64) {
+	u.mu.Lock()
+	defer u.mu.Unlock()
 	u.Balance += amount
 }
 
 func (u *User) Withdraw(amount float64) error {
+	u.mu.Lock()
+	defer u.mu.Unlock()
 	if u.Balance < amount {
 		return errors.New("Недостаточно средств на счету")
 	}
